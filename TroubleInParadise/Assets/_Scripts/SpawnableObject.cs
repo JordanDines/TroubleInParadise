52077cea28ad41d5b63a902acccb08d17a72398f
AIE\s171649 <eddie.ultra@gmail.com>
2018-03-05 14:36:03 +1100

    spawners and spawnable objects

---
+++ TroubleInParadise/Assets/_Scripts/SpawnableObject.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnableObject : MonoBehaviour
+{
+    //private rigibody variable
+    Rigidbody2D rb;
+    //private bool for checking if the player is holding something
+    bool isHeld = false;
+    //default value for parent
+    GameObject storeParent = null;
+    //the distance from the player the object will be.
+    public Vector2 distanceFromPlayer = new Vector2(1, 0);
+
+    //Awake function
+    void Awake()
+    {
+        //initilises the rigidbody
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    //updates the spawnable objects
+    void Update()
+    {
+        //checks if the object has been picked up
+        if (isHeld && storeParent != null)
+        {
+            rb.bodyType = RigidbodyType2D.Kinematic;
+            int dir = storeParent.GetComponent<Player>().GetDirection();
+            this.transform.position = storeParent.transform.position + new Vector3((float)dir * distanceFromPlayer.x, distanceFromPlayer.y, 0);
+        }
+        //resets dropped object
+        else
+        {
+            rb.bodyType = RigidbodyType2D.Dynamic;
+            this.transform.SetParent(null);
+        }
+
+    }
+    //trigger event to check if the player is close enough to the gameobject
+    void OnTriggerEnter2D(Collider2D other)
+    {
+    if(other.gameObject == GameObject.FindGameObjectWithTag("Player") && other.gameObject != this.gameObject)
+        {
+            this.transform.SetParent(other.transform);
+            storeParent = other.gameObject;
+            isHeld = true;
+        }   
+    }
+    //Getter
+    public bool getIsHeld()
+    {
+        return isHeld;
+    }
+    //Setter
+    public void setIsheld(bool holding)
+    {
+        isHeld = holding;
+    }
+}
---
+++ TroubleInParadise/Assets/_Scripts/SpawnerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerController : MonoBehaviour
+{
+    //This is the array of spawners:
+    // - Should be empty gaemObjects or can be solid object depending on the scene.
+    // - Holds the transforms of the locations the spawnable objects spawn in.
+    [Header("Spawners")]
+    [Tooltip("Spawner array")]
+    public GameObject[] Spawners;
+
+    //Array of Pickups:
+    // - holds all pickups that can be spawned.
+    [Header("Pickups")]
+    [Tooltip("add all objects that can be spawned in this array")]
+    public GameObject[] pickupArray;
+
+    //Spawn rate of spawnable objects in seconds
+    [Header("variables")]
+    [Tooltip("how often items spawn in seconds")]
+    public float spawnRate = 2.0f;
+
+    //holds a count as a default value for the counter
+    private float m_counter = 0.0f;
+
+    //update function, updates the main controller for the spawner
+    void Update()
+    {
+        //increments counter in seconds
+        m_counter += Time.deltaTime;
+        //checks if enough time has passed to spawn in an object
+        if (m_counter >= spawnRate)
+        {
+            //random digit for the spawn location
+            int selectedPoint = Random.Range(0, Spawners.Length);
+            //random digit for drop picking
+            int randDrop = Random.Range(0, pickupArray.Length);
+            //creates the spawnable object in the scene
+            Instantiate(pickupArray[randDrop], Spawners[selectedPoint].transform);
+            //resets counetr
+            m_counter = 0.0f;
+        }
+
+    }
+}
