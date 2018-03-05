0239d06295cba5c7a9c30a2601de3a6943f4bda8
AIE\s171649 <eddie.ultra@gmail.com>
2018-03-05 16:22:12 +1100

    fixed thievery bug

--- TroubleInParadise/Assets/_Scripts/SpawnableObject.cs
+++ TroubleInParadise/Assets/_Scripts/SpawnableObject.cs
@@ -8,11 +8,13 @@ public class SpawnableObject : MonoBehaviour
     Rigidbody2D rb;
     //private bool for checking if the player is holding something
     bool isHeld = false;
+
+    bool canHold = true;
     //default value for parent
     GameObject storeParent = null;
     //the distance from the player the object will be.
     public Vector2 distanceFromPlayer = new Vector2(1, 0);
-
+   
     public string playersTag = "Player";
 
     //Awake function
@@ -20,13 +22,14 @@ public class SpawnableObject : MonoBehaviour
     {
         //initilises the rigidbody
         rb = this.GetComponent<Rigidbody2D>();
+
     }
 
     //updates the spawnable objects
     void Update()
     {
         //checks if the object has been picked up
-        if (isHeld && storeParent != null)
+        if (isHeld && storeParent != null && !canHold)
         {
             rb.bodyType = RigidbodyType2D.Kinematic;
             int dir = storeParent.GetComponent<Player>().GetDirection();
@@ -37,19 +40,30 @@ public class SpawnableObject : MonoBehaviour
         {
             rb.bodyType = RigidbodyType2D.Dynamic;
             this.transform.SetParent(null);
+            canHold = true;
         }
 
     }
     //trigger event to check if the player is close enough to the gameobject
     void OnTriggerEnter2D(Collider2D other)
     {
-    if(other.gameObject == GameObject.FindGameObjectWithTag(playersTag) && other.gameObject != this.gameObject)
+        if (other.gameObject == GameObject.FindGameObjectsWithTag(playersTag)[0] && other.gameObject != this.gameObject && canHold)
         {
             this.transform.SetParent(other.transform);
             storeParent = other.gameObject;
+            canHold = false;
             isHeld = true;
-        }   
-    }
+        }
+
+        if (other.gameObject == GameObject.FindGameObjectsWithTag(playersTag)[1] && other.gameObject != this.gameObject && canHold)
+        {
+            this.transform.SetParent(other.transform);
+            storeParent = other.gameObject;
+            canHold = false;
+            isHeld = true;
+        }
+    }   
+    
     //Getter
     public bool getIsHeld()
     {
