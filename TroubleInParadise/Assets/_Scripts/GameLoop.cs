7e44721608453718f34a75998192b55bce6f851d
Jordan Dines <jordan@dines.net.au>
2018-03-06 21:59:11 +1100

    Added Controls Screen

--- TroubleInParadise/Assets/_Scripts/GameLoop.cs
+++ TroubleInParadise/Assets/_Scripts/GameLoop.cs
@@ -5,12 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class GameLoop : MonoBehaviour
 {
+
+	public GameObject controlsPanel;
+	public GameObject menuPanel;
+
     public string gameScene = "_GameScene";
     public string menuScene = "_MenuScene";
 
 	void Awake () {
 		DontDestroyOnLoad (transform.gameObject);
 	}
+
+	public void GoToControlsPanel(){
+		menuPanel.SetActive (false);
+		controlsPanel.SetActive (true);
+	}
+
+	public void GoToMenuPanel(){
+		menuPanel.SetActive (true);
+		controlsPanel.SetActive (false);
+	}
+
 
     //goes to the mian game scene
     public void GoToGameScene()
