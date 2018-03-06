a7944c293e55fe58c8fa93a4eef541047051af68
AIE\s171649 <eddie.ultra@gmail.com>
2018-03-06 15:10:33 +1100

    added scene moving script

---
+++ TroubleInParadise/Assets/_Scripts/GameLoop.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameLoop : MonoBehaviour
+{
+    public string mainScene = "_MainScene";
+    public string startScene = "GameScene";
+    public string endScene = "EndScene";
+
+    //goes to the mian game scene
+    public void GoToGameScene()
+    {
+        SceneManager.LoadScene(mainScene);
+    }
+    
+    //ending scene
+    public void GoToStartScene()
+    {
+        SceneManager.LoadScene(startScene);
+    }
+
+    //start scene
+    public void GoToEndScene()
+    {
+        SceneManager.LoadScene(endScene);
+    }
+}
