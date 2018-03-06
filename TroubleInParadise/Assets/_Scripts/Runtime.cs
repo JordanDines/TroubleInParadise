3f803f41ca2181c881e9e5ce92b49823ce447a61
AIE\s171649 <eddie.ultra@gmail.com>
2018-03-06 16:05:22 +1100

    runtime can now control a slider

--- TroubleInParadise/Assets/_Scripts/Runtime.cs
+++ TroubleInParadise/Assets/_Scripts/Runtime.cs
@@ -1,11 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Runtime : MonoBehaviour
 {
     [Header("The total time the game needs to run")]
-    [SerializeField] float totalFallTime;
+    public float totalFallTime = 120.0f;
+
+    //current percentage of slider
+    //private float currentpercentage = 0.0f;
+
+    public Slider fallingslider;
+    
+    //changes at which points the sprite changes
+    public float firstPointPercent = 0.0f;
+    public float secondPointPercent = 0.5f;
+    public float thirdPointPercent = 1.0f;
+
+    //3 different objects that slider head becomes
+    public GameObject firstSprite = null;
+    public GameObject secondSprite = null;
+    public GameObject thirdSprite = null;
+    //the knob
+    public GameObject sliderHeadSprite = null;
+
     [Header("The 2 players")]
     [SerializeField] Player player1;
     [SerializeField] Player player2;
@@ -20,21 +39,24 @@ public class Runtime : MonoBehaviour
     private Player playerThatWon;
 
     private int gameOver;
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        fallingslider.value = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
     {
+        //updates the current percentage
+        slidervalue();
+
         gameTime += Time.deltaTime;
         //Trigger end scene
         if (gameTime >= totalFallTime)
         {
             //Player 1 has the parachute
-            if(player1.HasParachute() == 1)
+            if (player1.HasParachute() == 1)
             {
                 playerThatWon = player1;
             }
@@ -50,7 +72,7 @@ public class Runtime : MonoBehaviour
             }
 
             //locks player controls and sets velocity of the x to 0
-            if(player1 != playerThatWon)
+            if (player1 != playerThatWon)
             {
                 player1.LockControls(fallSpeedWithoutParachute);
             }
@@ -58,7 +80,7 @@ public class Runtime : MonoBehaviour
             {
                 player1.LockControls(fallSpeedWithParachute);
             }
-            if(player2 != playerThatWon)
+            if (player2 != playerThatWon)
             {
                 player2.LockControls(fallSpeedWithoutParachute);
             }
@@ -70,7 +92,7 @@ public class Runtime : MonoBehaviour
             bottomBoxCollider.enabled = false;
             gameOver = 1;
         }
-        if(gameOver == 1)
+        if (gameOver == 1)
         {
             float cameraZPos = Camera.main.transform.position.z;
             if (player1 == playerThatWon)
@@ -80,14 +102,14 @@ public class Runtime : MonoBehaviour
                     Camera.main.transform.position = new Vector3(0.0f, player2.transform.position.y, cameraZPos);
                 }
             }
-            else if(player2 == playerThatWon)
+            else if (player2 == playerThatWon)
             {
                 if (Camera.main.transform.position.y > player1.transform.position.y)
                 {
                     Camera.main.transform.position = new Vector3(0.0f, player1.transform.position.y, cameraZPos);
                 }
             }
-            else if(player1 != playerThatWon && player2 != playerThatWon)
+            else if (player1 != playerThatWon && player2 != playerThatWon)
             {
                 float averageYPos = (player2.transform.position.y + player1.transform.position.y) / 2;
                 //
@@ -97,5 +119,42 @@ public class Runtime : MonoBehaviour
                 }
             }
         }
-	}
+    }
+
+    //Updates the current percentage
+    public void slidervalue()
+    {
+        float singlePercentage = 100.0f/totalFallTime;
+        fallingslider.value = gameTime * singlePercentage * Time.deltaTime;
+        firstVal();
+        secondVal();
+        thirdVal();
+        if (fallingslider.value > 1.0f)
+            fallingslider.value = 1.0f;
+
+    }
+    //first value effect
+    public void firstVal()
+    {
+        if (fallingslider.value >= firstPointPercent && fallingslider.value < secondPointPercent)
+        {
+            sliderHeadSprite = firstSprite;
+        }
+    }
+    //second value effect
+    public void secondVal()
+    {
+        if (fallingslider.value >= secondPointPercent && fallingslider.value < thirdPointPercent)
+        {
+            sliderHeadSprite = secondSprite;
+        }
+    }
+    //third value effect
+    public void thirdVal()
+    {
+        if (fallingslider.value >= thirdPointPercent)
+        {
+            sliderHeadSprite = thirdSprite;
+        }
+    }
 }
